52a5c903f867963051722312b4a8c165b3a89fad
Martin Lippera <martinlippera@gmail.com>
2025-12-15 15:30:33 -0300

    Agregar innerHTML para navbar homepage

---
+++ frontend/js/navbar.js
@@ -0,0 +1,26 @@
+const currentPage = window.location.pathname;
+const navbarContainer = document.querySelector('.navbar');
+
+if (currentPage.includes('index.html') || currentPage.endsWith('/')) {
+    navbarContainer.innerHTML = `
+   <div class="navbar-links">
+        <a class="navbar-brand" href="./index.html">Home</a>
+        <a class="navbar-brand" href="./leaderboard.html">Tabla</a>
+        <a class="navbar-brand" href="./profile.html">Mi perfil</a>
+      </div>
+
+      <form class="d-flex" role="search">
+        <input
+          class="form-control"
+          type="search"
+          placeholder="Buscar en tus post"
+          aria-label="Search"
+        />
+        <button class="btn btn-primary" type="submit">Buscar</button>
+      </form> 
+  `
+} else if (currentPage.includes('leaderboard.html')) {
+
+} else if (currentPage.includes('profile.html')) {
+
+}
