35915295b977f93583803fe529ac1d08ef033150
Martin Lippera <martinlippera@gmail.com>
2025-12-15 22:13:23 -0300

    Unificar navbar al estilo del perfil

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -3,37 +3,31 @@ const navbarContainer = document.querySelector('.navbar');
 
 if (currentPage.includes('index.html') || currentPage.endsWith('/')) {
     navbarContainer.innerHTML = `
-   <div class="navbar-links">
-        <a class="navbar-brand" href="./index.html">Home</a>
-        <a class="navbar-brand" href="./leaderboard.html">Tabla</a>
-        <a class="navbar-brand" href="./profile.html">Mi perfil</a>
-      </div>
+      <a class="navbar-brand" href="./index.html">Homepage</a>
+      <a class="navbar-brand" href="./leaderboard.html">Tabla</a
+      <a class="navbar-brand" href="./profile.html">Perfil</a>
 
       <form class="d-flex" role="search">
-        <input
-          class="form-control"
-          type="search"
-          placeholder="Buscar en tus post"
-          aria-label="Search"
-        />
-        <button class="btn btn-primary" type="submit">Buscar</button>
-      </form> 
+        <input class="form-control" type="search" placeholder="Buscar" aria-label="Search"/>
+        <button class="boton" type="submit">Buscar</button>
+      </form>
   `
 } else if (currentPage.includes('leaderboard.html')) {
   navbarContainer.innerHTML = `
-      <div class="navbar-links">
-        <a class="navbar-brand" href="./index.html">
-          <i class="bi bi-house-fill"></i> Home
-        </a>
-        <a class="navbar-brand" href="./profile.html">
-          <i class="bi bi-person-circle"></i> Mi perfil
-        </a>
-      </div>
+      <a class="navbar-brand" href="./index.html">Homepage</a>
+      <a class="navbar-brand" href="./leaderboard.html">Tabla</a
+      <a class="navbar-brand" href="./profile.html">Perfil</a>
+
+      <form class="d-flex" role="search">
+        <input class="form-control" type="search" placeholder="Buscar equipos" aria-label="Search"/>
+        <button class="boton" type="submit">Buscar</button>
+      </form>
   `
 } else if (currentPage.includes('profile.html')) {
   navbarContainer.innerHTML = `
       <a class="navbar-brand" href="./index.html">Homepage</a>
       <a class="navbar-brand" href="./leaderboard.html">Tabla</a>
+      <a class="navbar-brand" href="./profile.html">Perfil</a>
 
       <form class="d-flex" role="search">
         <input class="form-control" type="search" placeholder="Buscar en tus post" aria-label="Search"/>
